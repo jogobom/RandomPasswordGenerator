7968aecc21f83970369f235c9d688c2aea3c0ad9
Chris Preston <chris_preston@waters.com>
2021-05-17 15:03:28 +0100

    Add first SpecFlow scenario

---
+++ RandomPasswordGenerator.Specs/Features/RandomPassword.feature
@@ -0,0 +1,6 @@
+﻿Feature: RandomPassword
+	As an internet user I want a secure password so that I don't get hacked
+
+Scenario: Password should be the required length
+	When I generate a password
+	Then the password will be 32 characters long
---
+++ RandomPasswordGenerator.Specs/Steps/PasswordStepDefinitions.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace RandomPasswordGenerator.Specs.Steps
+{
+    [Binding]
+    public sealed class PasswordStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private const string PasswordKey = "password";
+
+        private readonly ScenarioContext myScenarioContext;
+
+        public PasswordStepDefinitions(ScenarioContext scenarioContext)
+        {
+            this.myScenarioContext = scenarioContext;
+        }
+
+        [When(@"I generate a password")]
+        public void WhenIGenerateAPassword()
+        {
+            var password = RandomPasswordGeneratorTest.RandomPasswordGenerator.GeneratePassword();
+            myScenarioContext[PasswordKey] = password;
+        }
+
+        [Then(@"the password will be (.*) characters long")]
+        public void ThenThePasswordWillBeCharactersLong(int requiredLength)
+        {
+            var password = (string)myScenarioContext[PasswordKey];
+            password.Length.Should().Be(requiredLength);
+        }
+    }
+}
